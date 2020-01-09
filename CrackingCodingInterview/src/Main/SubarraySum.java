42db44d2e2973874c952de39813df4c9b9618a49
steph <steph@DESKTOP-OEN7EG9>
2020-01-08 23:14:21 -0600

    Successfully solved SubarraySum. TimeComplexity = O(n) Space Complexity O(n)

--- CrackingCodingInterview/src/Main/SubarraySum.java
+++ CrackingCodingInterview/src/Main/SubarraySum.java
@@ -1,25 +1,26 @@
 package Main;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class SubarraySum {
 	
 	public int subarraySum(int[] nums, int k) {
 		
+		Map<Integer,Integer> storage = new HashMap<>();
 		
-		
-		return 0;
-    }
-	
-	private void helperSum(int[] nums, int index, int sum, List<Integer> count) {
-		
-		if(index >= nums.length) {
-			
+		int total = 0;
+		int count = 0;
+		storage.put(0,1);
+		for(int i = 0; i<nums.length;i++) {
+			total += nums[i];
+			if(storage.containsKey(total-k)) {
+				count+= storage.get(total-k);
+			}
+			storage.merge(total, 1, Integer::sum);
 		}
 		
-		
-		
-		
-	}
-	
+		return count;
+    }	
 }
